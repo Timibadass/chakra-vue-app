b3be43a08192a6ef9945f059b101dabafaa372ab
timibadass <omoyenitimi@gmail.com>
2020-07-17 09:51:37 +0100

    setup theme for nuxt app

--- nuxt/nuxt.config.js
+++ nuxt/nuxt.config.js
@@ -1,54 +1,17 @@
 import { faMedal } from '@fortawesome/free-solid-svg-icons'
+// import the theme
+import themeFile from './plugins/theme-file'
 export default {
-    /*
-     ** Nuxt rendering mode
-     ** See https://nuxtjs.org/api/configuration-mode
-     */
-    mode: 'universal',
-    /*
-     ** Headers of the page
-     ** See https://nuxtjs.org/api/configuration-head
-     */
-    head: {
-        title: process.env.npm_package_name || '',
-        meta: [
-            { charset: 'utf-8' },
-            { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-            { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
-        ],
-        link: [
-            { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-        ]
-    },
-    /*
-     ** Global CSS
-     */
-    css: [],
-    /*
-     ** Plugins to load before mounting the App
-     ** https://nuxtjs.org/guide/plugins
-     */
-    plugins: [],
-    /*
-     ** Nuxt.js dev-modules
-     */
-    buildModules: [],
-    /*
-     ** Nuxt.js modules
-     */
     modules: [
         '@chakra-ui/nuxt',
         '@nuxtjs/emotion'
     ],
     chakra: {
+        // add it to chakra config
+        extendTheme: themeFile,
         icons: {
             iconPack: 'fa',
             iconSet: { faMedal }
         }
-    },
-    /*
-     ** Build configuration
-     ** See https://nuxtjs.org/api/configuration-build/
-     */
-    build: {}
+    }
 }
