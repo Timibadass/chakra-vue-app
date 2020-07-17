725b6c8df8900dde4c9eb4cd67949d114ac97e1b
timibadass <omoyenitimi@gmail.com>
2020-07-17 09:44:30 +0100

    add theme to app

--- vue/src/main.js
+++ vue/src/main.js
@@ -2,9 +2,14 @@ import Vue from 'vue'
 import App from './App.vue'
 import Chakra, { CThemeProvider, CReset } from '@chakra-ui/vue'
 
+// import the file
+import themeFile from './theme-file'
+
 import { faMedal } from '@fortawesome/free-solid-svg-icons'
 
 Vue.use(Chakra, {
+    // add it to the extendTheme property
+    extendTheme: themeFile,
     icons: {
         iconPack: 'fa',
         iconSet: { faMedal }
---
+++ vue/src/theme-file.js
@@ -0,0 +1,10 @@
+export default {
+    colors: {
+        gray: {
+            50: "#EDFDFD",
+            80: "#C4F1F9",
+            110: "#9DECF9",
+            130: "#76E4F7"
+        }
+    }
+};
